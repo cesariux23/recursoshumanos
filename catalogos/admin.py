815e30f28a5f6a2e597259d475160419b4990fca
Cèsar Encarnación <cesariux23@gmail.com>
2017-06-30 23:55:37 -0500

    add first model to admin app

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Adscripcion, Plaza, Persona
 
 # Register your models here.
+admin.site.register(Adscripcion)
+admin.site.register(Plaza)
+admin.site.register(Persona)
