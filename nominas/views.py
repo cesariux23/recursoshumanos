3f911accc46269d133929a885b188a5614c55b00
Cèsar Encarnación <cesariux23@gmail.com>
2017-07-03 00:17:53 -0500

    first login

--- nominas/views.py
+++ nominas/views.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-
+@login_required(login_url="login/")
 def index(request):
     return render(request, "nominas/dashboard.html",{})
--- recursoshumanos/urls.py
+++ recursoshumanos/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login),
     url(r'^admin/', admin.site.urls),
     url(r'^nominas/', include('nominas.urls')),
 ]
